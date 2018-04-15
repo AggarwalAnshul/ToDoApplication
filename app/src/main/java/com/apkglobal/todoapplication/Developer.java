39d9f7f17233e2aedda9a34be690f1e963665270
AggarwalAnshul <anshul.aggarwal.sfd.2@gmail.com>
2018-04-15 21:58:25 +0530

    New layout, UI Improvements, Added Animations, Removed to ListView, Night

---
+++ app/src/main/java/com/apkglobal/todoapplication/Developer.java
@@ -0,0 +1,20 @@
+package com.apkglobal.todoapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Developer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developer);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+}
